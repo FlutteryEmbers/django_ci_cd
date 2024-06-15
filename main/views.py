7dba633568020420b23f865320f37ed55dc1d9c9
shili.wu <shili.wu@hotmail.com>
2024-06-15 10:35:27 -0500

    new_title

--- main/views.py
+++ main/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home(request):
-    return HttpResponse('Hello, welcome to my Django site!')
+    return HttpResponse('Hello, welcome to my Django site Modified!')
 # Create your views here.
